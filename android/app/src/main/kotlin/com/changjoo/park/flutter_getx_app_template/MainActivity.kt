1f40ca615587a3bbc97a4fd269e341bf24b281d0
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-01-11 09:09:20 +0900

    Initialize Commit

---
+++ android/app/src/main/kotlin/com/changjoo/park/flutter_getx_app_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.changjoo.park.flutter_getx_app_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'package:flutter_getx_app_template/app/modules/home/controllers/home_controller.dart';
+
+class HomeBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+  }
+}
